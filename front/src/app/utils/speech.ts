f9f452dfb8bc52524745c1d9460919025f4ec48c
wkhr-m <midoriri41@gmail.com>
2022-08-19 09:47:25 +0900

    fix voice

--- front/src/app/utils/speech.ts
+++ front/src/app/utils/speech.ts
@@ -7,6 +7,7 @@ export const speechWord = (
   const uttr = new SpeechSynthesisUtterance();
   uttr.text = word;
   uttr.lang = lang;
+  uttr.voice = window.speechSynthesis.getVoices()[1];
   uttr.rate = rate;
   window.speechSynthesis.speak(uttr);
 };
