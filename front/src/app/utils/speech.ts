98172740dc932fca4848df49d6d2f69d787d3b2f
wkhr-m <midoriri41@gmail.com>
2022-08-19 20:35:54 +0900

    TTSのvoiceの言語を設定

--- front/src/app/utils/speech.ts
+++ front/src/app/utils/speech.ts
@@ -7,7 +7,9 @@ export const speechWord = (
   const uttr = new SpeechSynthesisUtterance();
   uttr.text = word;
   uttr.lang = lang;
-  uttr.voice = window.speechSynthesis.getVoices()[1];
+  uttr.voice = window.speechSynthesis
+    .getVoices()
+    .filter((item) => item.lang === lang)[0];
   uttr.rate = rate;
   window.speechSynthesis.speak(uttr);
 };
