1de229e4a0edd9c5d7ed063782e72709a904c01d
wkhr-m <midoriri41@gmail.com>
2022-09-09 16:43:49 +0900

    容量を表示するようにした

--- front/src/app/parts/download-book/download-book.component.ts
+++ front/src/app/parts/download-book/download-book.component.ts
@@ -1,5 +1,5 @@
 import { DialogRef } from '@angular/cdk/dialog';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { REGEXP_SPREADSHEET_URL } from '@utils/regexp';
@@ -14,13 +14,31 @@ export type DownloadBookComponentOutput = {
   templateUrl: './download-book.component.html',
   styleUrls: ['./download-book.component.scss'],
 })
-export class DownloadBookComponent {
+export class DownloadBookComponent implements OnInit {
+  capacityMsg: string = '';
+  isCapacityOver: boolean = true;
+  isNearCapacity: boolean = false;
   form = new FormGroup({
     url: new FormControl('', Validators.compose([Validators.required])),
     name: new FormControl('シート1', Validators.compose([Validators.required])),
   });
 
   constructor(public dialogRef: DialogRef, private _snackBar: MatSnackBar) {}
+
+  async ngOnInit() {
+    if (!!navigator.storage.estimate) {
+      const storage = await navigator.storage.estimate();
+      const all = (storage.quota || 0) * 0.8;
+      const usage = storage.usage || 0;
+      const rate = Math.round((usage * 100) / all);
+      this.capacityMsg = `現在${rate}%の容量が使用されています。`;
+      if (rate > 99) {
+        this.isCapacityOver = true;
+      } else if (rate > 80) {
+        this.isNearCapacity = true;
+      }
+    }
+  }
 
   onClose(): void {
     this.dialogRef.close();
